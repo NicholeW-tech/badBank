3d93034c820651689a70d094829cf49a3d25a4cb
NicholeW-tech <85132041+NicholeW-tech@users.noreply.github.com>
2022-08-18 15:04:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ passport.serializeUser(function(user, done) {
    passport.use(
     new GoogleStrategy(
      {
-      clientID: "991978049821-viuktd5jvhc3qojkr0psjbatauk467ld.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientID: CLIENTID,
+      clientSecret: CLIENTSECRET,
       callbackURL: "https://badbanktest1.herokuapp.com/auth/google/callback"
      },
      function(accessToken, refreshToken, profile, done) {
